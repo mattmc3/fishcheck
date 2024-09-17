dc31f03adb0fdced18007c72754315844e9d8ff7
mattmc3 <mattmc3@gmail.com>
2024-09-17 10:26:57 -0400

    WIP

---
+++ checks/FCW2001.fish
@@ -0,0 +1,29 @@
+function FCW2001 -d "Local variable redeclared, and possibly shadows prior scope."
+    # set -l exitcode 0
+    # set -l pat '\bset\s+-(?:l|-local)\s*(\w+)'
+    # set -l seen_vars
+    # for fishfile in $argv
+    #     set -l lineno 0
+    #     while read -l line
+    #         set lineno (math $lineno + 1)
+    #         set var_name (string match -rg -- $pat $line)
+    #         if test $status -eq 0
+    #             if contains $var_name $seen_vars
+    #                 set exitcode 1
+    #                 set -l position (string match -rn -- $pat $line | string split ' ')
+    #                 test $status -eq 0 || set position 0 0
+    #                 _fishcheck_report \
+    #                     --file $fishfile \
+    #                     --check FCW2000 \
+    #                     --lineno $lineno \
+    #                     --line $line \
+    #                     -S $position[1] -L $position[2] \
+    #                     --did-you-mean "set $var_name ..."
+    #             else
+    #                 set --append seen_vars $var_name
+    #             end
+    #         end
+    #     end <$fishfile
+    # end
+    # return $exitcode
+end
